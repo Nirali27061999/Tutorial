fcfbe74ff9d1723e25dd5c05111a064012b00ceb
Nirali27061999 <71005588+Nirali27061999@users.noreply.github.com>
2020-09-09 14:05:40 +0530

    Add files via upload

---
+++ Branchdetails.aspx
@@ -0,0 +1,149 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Branchdetails.aspx.cs" Inherits="WebApplication4.Branchdetails" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="Bootstrap/css/bootstrap.css" rel="stylesheet" />
+    <link rel="stylesheet" href="StyleSheet2.css" type="text/css" />
+     <script type = "text/javascript">
+      function Confirm() {
+          var confirm_value = document.createElement("INPUT");
+          confirm_value.type = "hidden";
+          confirm_value.name = "confirm_value";
+          if (confirm("Do you want to change?")) {
+              confirm_value.value = "Yes";
+          } else {
+              confirm_value.value = "No";
+          }
+          document.forms[0].appendChild(confirm_value);
+      }
+     </script>
+</head>
+<body>
+    <form id="form1" runat="server" autocomplete="off">
+        <center>
+        <h2 class="text-danger col-lg-4 col-md-10 col-sm-12">MAS Finicial LTD.</h2></center>
+       
+        <br />
+        <br />
+        <div>
+            <center>
+                <div class="text-dark" style="margin-top:50px;">
+                <h2 class="text-capitalize text-info">Branch Form</h2><hr />
+            </div>
+                <div class="row">  
+                    <div class="col-lg-3"></div>  
+                    <div class="col-lg-6">  
+                <div class="form-group">
+     <asp:HiddenField ID="HiddenField1" runat="server" />
+    </div>
+                <div class="form-group">
+     <asp:Label ID="lblbmname" runat="server" Text="Branch Name:*" CssClass="col-lg-3 col-md-6 col-sm-12"></asp:Label>
+      <asp:TextBox ID="txtbmname" runat="server" CssClass=" col-lg-3 col-md-6 col-sm-12" Width="200px"  MaxLength="20" placeholder="Branch Name*"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="reqbmname" runat="server" ControlToValidate="txtbmname" ErrorMessage="Please Fill Branch name" ForeColor="#ff0000"></asp:RequiredFieldValidator>
+                        <asp:RegularExpressionValidator ID="ragbmname" runat="server"  ControlToValidate="txtbmname" ErrorMessage="Please enter only alphabets" ValidationExpression="[a-zA-Z ]*$" ForeColor="#ff0000"></asp:RegularExpressionValidator>
+    </div>
+    <div class="form-group">
+       <asp:Label ID="lblisactive" runat="server" Text="Is active?:*" CssClass="col-lg-3 col-md-6 col-sm-12" ></asp:Label>
+      <asp:RadioButton ID="rbyes" runat="server" Text="Yes" Checked="true" GroupName="active" CssClass="custom-radio col-lg-3 col-md-6 col-sm-12 " />
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <asp:RadioButton ID="rbno" runat="server" Text="No" GroupName="active" />
+      
+    </div>
+                <div class="form-group">
+                     <asp:Button ID="btnsbt" runat="server" Text="Submit" OnClick="btnsbt_Click" CssClass="btn btn-primary"  />
+                    <asp:Button ID="btnclear" runat="server" Text="Clear" OnClick="btnclear_Click" CssClass="btn btn-danger"  />
+       
+    </div>
+      
+    </div>
+    
+                  <%--<asp:HiddenField ID="HiddenField1" runat="server" />
+            <table class="table table-responsive">
+                <tr>
+                    <td class="auto-style1">
+                        <asp:Label ID="lblbmname" runat="server" Text="Branch Name:*" CssClass="name"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtbmname" runat="server" CssClass="form-control" MaxLength="20" placeholder="Branch Name*"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="reqbmname" runat="server" ControlToValidate="txtbmname" ErrorMessage="Please Fill Branch name" ForeColor="#ff0000"></asp:RequiredFieldValidator>
+                        <asp:RegularExpressionValidator ID="ragbmname" runat="server"  ControlToValidate="txtbmname" ErrorMessage="Please enter only alphabets" ValidationExpression="[a-zA-Z ]*$" ForeColor="#ff0000"></asp:RegularExpressionValidator>
+
+                    </td>
+                    </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblisactive" runat="server" Text="Is active?:*" ></asp:Label>
+                    </td>
+                    <td>
+-                        <asp:RadioButton ID="rbyes" runat="server" Text="Yes" Checked="true" GroupName="active" />
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <asp:RadioButton ID="rbno" runat="server" Text="No" GroupName="active" />
+                    </td>
+
+                </tr>
+                 <tr>
+                    <td colspan="4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <asp:Button ID="btnsbt" runat="server" Text="Submit" OnClick="btnsbt_Click" CssClass="btn btn-primary"  />
+                        &nbsp;&nbsp;
+                        &nbsp;&nbsp;
+                        <asp:Button ID="btnclear" runat="server" Text="Clear" OnClick="btnclear_Click" CssClass="btn btn-danger"  />
+                    </td>
+                </tr>
+                </table>--%>
+                 <br />
+       
+        </center>
+         <center>
+            <div id="divline">
+                <h3>Gridview details</h3>
+            </div>
+             </center>
+             <br />
+             <div class="grid">
+             <strong> Search Branch:</strong>
+        <asp:TextBox ID="txtsearch" runat="server" CssClass="form-control" Width="200px" OnTextChanged="txtsearch_TextChanged" AutoPostBack="true"></asp:TextBox>
+                 </div>
+             <br />
+            <asp:GridView ID="GridView1" runat="server" CssClass="table table-responsive" Width="65%" HeaderStyle-BackColor="#ff6600" AutoGenerateColumns="False" PageSize="10" AllowPaging="true" OnRowDataBound="GridView1_RowDataBound" OnRowEditing="GridView1_RowEditing" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowCommand="GridView1_RowCommand" OnRowDeleting="GridView1_RowDeleting" >
+
+                <Columns>
+                    <asp:TemplateField HeaderText="Row Number" ItemStyle-Width="100">
+                        <ItemTemplate>
+                            <asp:Label ID="lblRowNumber" Text='<%# Container.DataItemIndex + 1 %>' runat="server" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:BoundField DataField="branchname" HeaderText="Branch name" SortExpression="branchname" />
+
+                    <asp:TemplateField HeaderText="IsActive" SortExpression="isactive">
+                        <ItemTemplate>
+                            <asp:Button BackColor="Transparent" BorderStyle="None" ID="btnIsActive" CommandName="ActiveClick" CommandArgument='<%# Eval("id")%>' OnClientClick="Confirm()" CausesValidation="false" Text='<%# (Boolean.Parse(Eval("isactive").ToString())) ? "yes" : "no" %>' runat="server" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Actions" ItemStyle-Width="50">
+                        <ItemTemplate>
+                            <asp:ImageButton ImageUrl="~/images/edit iamge.jpg" CommandName="Edit" ToolTip="Edit" runat="server" Width="20px" Height="20px" CausesValidation="false" CommandArgument='<%# Eval("id") %>' />
+                            <asp:ImageButton ImageUrl="~/images/delete-button_318-77600.jpg" CommandName="Delete" ToolTip="Delete" runat="server" Width="20px" Height="20px" CausesValidation="false" CommandArgument='<%# Eval("id") %>' />
+                        </ItemTemplate>
+                      
+                    </asp:TemplateField>
+
+
+                </Columns>
+
+            </asp:GridView>
+                
+                </center>
+    </form>
+</body>
+</html>
---
+++ Branchdetails.aspx.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Specialized;
+
+namespace WebApplication4
+{
+    public partial class Branchdetails : System.Web.UI.Page
+    {
+        String Connection = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                fillgridview();
+            }
+        }
+        void fillgridview()
+        {
+            string user = string.Empty;
+            SqlConnection con = new SqlConnection(Connection);
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("branchviewall", con);
+            sda.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sda.SelectCommand.Parameters.AddWithValue("@isall", 0);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+                if (Convert.ToInt32(dt.Rows[0]["isactive"]) == 0)
+                {
+                    user = "no";
+                }
+                else
+                {
+                    user = "yes";
+                }
+            }
+            else
+            {
+                dt.Rows.Add(dt.NewRow());
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+                GridView1.Rows[0].Cells.Clear();
+                GridView1.Rows[0].Cells.Add(new TableCell());
+                GridView1.Rows[0].Cells[0].ColumnSpan = dt.Columns.Count;
+                GridView1.Rows[0].Cells[0].Text = "no data found";
+                GridView1.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+            con.Close();
+
+        }
+        public void clear()
+        {
+            txtbmname.Text = "";
+            rbyes.Checked = true;
+        }
+        protected void txtsearch_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtsearch.Text.Trim()))
+            {
+                fillgridview();
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection(Connection);
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand("branchsearch", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@branchname", txtsearch.Text.Trim());
+
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+        }
+
+
+
+
+        protected void btnsbt_Click(object sender, EventArgs e)
+        {
+            DateTime createdDate = DateTime.Now;
+
+            int active = 0;
+            if (rbyes.Checked)
+            {
+                active = 1;
+            }
+            try
+            {
+                if (HiddenField1.Value == "")
+                {
+                    SqlConnection con = new SqlConnection(Connection);
+
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("branchcreate", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", (HiddenField1.Value == "" ? 0 : Convert.ToInt32(HiddenField1.Value)));
+                    cmd.Parameters.AddWithValue("@branchname", txtbmname.Text.Trim());
+                    cmd.Parameters.AddWithValue("@createddate", createdDate);
+                    cmd.Parameters.AddWithValue("@modifieddate", 0);
+                    cmd.Parameters.AddWithValue("@isactive", active);
+                    cmd.Parameters.AddWithValue("@isdeleted", 0);
+
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    fillgridview();
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>confirm('Saved Sucessfully')</script>");
+                    clear();
+
+
+                }
+                else
+                {
+                    SqlConnection con = new SqlConnection(Connection);
+
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("branchupdate", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", (HiddenField1.Value == "" ? 0 : Convert.ToInt32(HiddenField1.Value)));
+                    cmd.Parameters.AddWithValue("@branchname", txtbmname.Text.Trim());
+                    cmd.Parameters.AddWithValue("@modifieddate", DateTime.Now);
+                    cmd.Parameters.AddWithValue("@isactive", active);
+
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    fillgridview();
+
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>confirm('Update Sucessfully')</script>");
+                    clear();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+
+
+        }
+
+        protected void btnclear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            fillgridview();
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int id = Convert.ToInt32(e.CommandArgument.ToString());
+
+
+            if (e.CommandName == "ActiveClick")
+            {
+                string confirmValue = Request.Form["confirm_value"];
+                if (confirmValue == "Yes")
+                {
+                    SqlConnection con = new SqlConnection(Connection);
+                    con.Open();
+
+                    SqlDataAdapter sdaQuery = new SqlDataAdapter("branchbyid", con);
+                    sdaQuery.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    sdaQuery.SelectCommand.Parameters.AddWithValue("@id", id);
+                    DataTable dtQuery = new DataTable();
+                    sdaQuery.Fill(dtQuery);
+                    int isactive = Convert.ToInt32(dtQuery.Rows[0]["isactive"]);
+
+                    SqlDataAdapter sda = new SqlDataAdapter("branchactivechange", con);
+                    sda.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    sda.SelectCommand.Parameters.AddWithValue("@isactive", 1 - isactive);
+                    sda.SelectCommand.Parameters.AddWithValue("@id", id);
+                    sda.SelectCommand.Parameters.AddWithValue("@modifieddate", DateTime.Now);
+
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    con.Close();
+                    fillgridview();
+                }
+                else
+                {
+                    fillgridview();
+                }
+            }
+            if (e.CommandName == "Edit")
+            {
+                SqlConnection con = new SqlConnection(Connection);
+                con.Open();
+
+                SqlDataAdapter sda = new SqlDataAdapter("branchbyid", con);
+                sda.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sda.SelectCommand.Parameters.AddWithValue("@id", id);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                con.Close();
+                HiddenField1.Value = id.ToString();
+                txtbmname.Text = dt.Rows[0]["branchname"].ToString();
+                if (Convert.ToInt32(dt.Rows[0]["isactive"]) == 0)
+                {
+                    rbno.Checked = true;
+                    rbyes.Checked = false;
+                }
+                else
+                {
+                    rbyes.Checked = true;
+                    rbno.Checked = false;
+                }
+            }
+            if (e.CommandName == "Delete")
+            {
+                SqlConnection con = new SqlConnection(Connection);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("branchdeletebyid", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+
+
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>confirm(' Delete Successfully ')</script>");
+                fillgridview();
+
+            }
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+
+        protected void btnIsActive_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
